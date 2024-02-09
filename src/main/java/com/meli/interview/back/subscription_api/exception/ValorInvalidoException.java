75499eb6bcba653a1d0d239c2d20578c5d5f452a
juan-para <juanpara86@gmail.com>
2024-02-09 09:31:50 -0300

    Se agrega excepcion custom

---
+++ src/main/java/com/meli/interview/back/subscription_api/exception/ValorInvalidoException.java
@@ -0,0 +1,7 @@
+package com.meli.interview.back.subscription_api.exception;
+
+public class ValorInvalidoException extends Exception {
+    public ValorInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
